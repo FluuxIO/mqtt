da1aee29e52f88116a63000a05940f58d033114d
Mickael Remond <mremond@process-one.net>
2016-05-08 16:08:39 +0200

    Clean example client receive loop

--- cmd/go_sub/go_sub.go
+++ cmd/go_sub/go_sub.go
@@ -10,10 +10,25 @@ import (
 func main() {
 	options := mqtt.ClientOptions{Address: "localhost:1883", Keepalive: 30}
 	fmt.Printf("Server to connect to: %s\n", options.Address)
+
 	client, _ := mqtt.NewClient(options)
 	statusChan := client.Connect()
-	<-statusChan
+
+	if s1 := <-statusChan; s1.Err != nil {
+		fmt.Printf("Connection error: %q\n", s1.Err)
+		return
+	}
+
 	topic := packet.Topic{Name: "test/topic"}
 	client.Subscribe(topic)
-	<-statusChan
+
+	for {
+		if s2 := <-statusChan; s2.Err != nil {
+			fmt.Printf("MQTT error: %q\n", s2.Err)
+			break
+		} else {
+			fmt.Printf("Received packet from Server: %+v\n", s2.Packet)
+		}
+	}
+
 }
