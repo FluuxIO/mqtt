28228a1d6647763bbda9644d056af6ff79ee5b25
Mickael Remond <mremond@process-one.net>
2016-05-23 12:17:50 +0200

    Allow setting clientID
    This is needed to support persistent sessions.

--- mqtt/packet/connect.go
+++ mqtt/packet/connect.go
@@ -25,6 +25,10 @@ func (c *Connect) SetKeepalive(keepalive int) {
 	c.keepalive = keepalive
 }
 
+func (c *Connect) SetClientID(clientID string) {
+	c.clientID = clientID
+}
+
 // Marshall return buffer containing serialized CONNECT MQTT control packet
 func (c *Connect) Marshall() bytes.Buffer {
 	var variablePart bytes.Buffer
@@ -37,7 +41,7 @@ func (c *Connect) Marshall() bytes.Buffer {
 	variablePart.WriteByte(byte(protocolLevel))
 	variablePart.WriteByte(byte(connectFlags))
 	variablePart.Write(encodeUint16(keepalive))
-	variablePart.Write(encodeString(defaultClientID)) // TODO: Support custom clientID
+	variablePart.Write(encodeString(setDefaultClientID(c.clientID)))
 
 	fixedHeader := (connectType<<4 | fixedHeaderFlags)
 	packet.WriteByte(byte(fixedHeader))
@@ -45,4 +49,11 @@ func (c *Connect) Marshall() bytes.Buffer {
 	packet.Write(variablePart.Bytes())
 
 	return packet
+}
+
+func setDefaultClientID(clientID string) string {
+	if clientID == "" {
+		return defaultClientID
+	}
+	return clientID
 }
