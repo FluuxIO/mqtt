274aebed38bfeaf07c3e99462f660d12b77cf1c5
Mickael Remond <mremond@process-one.net>
2018-01-06 17:54:26 +0100

    Retry closing listener

--- mqtt/client_test.go
+++ mqtt/client_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 	"time"
 
+	"fmt"
+
 	"fluux.io/gomqtt/mqtt"
 )
 
@@ -107,8 +109,16 @@ func (mock *MQTTServerMock) Start(t *testing.T, handler testHandler) {
 
 func (mock *MQTTServerMock) Stop() {
 	close(mock.done)
-	if mock.listener != nil {
-		mock.listener.Close()
+
+	for i := 0; i <= 10; i++ {
+		if mock.listener != nil {
+			if err := mock.listener.Close(); err != nil {
+				fmt.Println("cannot close listener", err)
+				continue
+			}
+			break
+		}
+
 	}
 	// Close all existing connections
 	for _, c := range mock.connections {
