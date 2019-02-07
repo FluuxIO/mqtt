e837f630a5d0eb4d6cecfa750614a21384c84239
Mickael Remond <mremond@process-one.net>
2019-02-07 09:17:03 +0100

    Minor improvement on doc

--- doc.go
+++ doc.go
@@ -4,5 +4,8 @@ Package mqtt implements MQTT client protocol. It can be used as a client library
 You can use the MQTT client directly at the low-level and handle connection events in your own code (or ignore them).
 If you want to have sane default behaviour for handling reconnect, you can directly rely on the connection manager
 struct.
+
+The messages are received on a message channel. The channel can be buffered. The main goal of the channel is to handle
+back pressure and make sure the client will not read message faster than it is able to process.
 */
 package mqtt // import "gosrc.io/mqtt"
